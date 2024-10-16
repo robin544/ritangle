c60d4d99310a3688eefc4c6909440cfdc8fae4e2
robin544 <r.choudhury@protonmail.com>
2024-10-16 10:10:43 +0100

    Update for i in range().py

--- for i in range().py
+++ for i in range().py
@@ -1,5 +1,5 @@
 # Three positive integers a, b and c where a < b < c satisfy:
 # abc + ab + bc + ca + a + b + c = 2024
 # Find the sum of all the possible different values of c
-
+import randint
 for i in range():
