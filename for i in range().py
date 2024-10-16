05ce02602fd341c00c39baa55e17bbb8a2ab82f4
Kai <kaijbooth@gmail.com>
2024-10-16 10:30:47 +0100

    Update for i in range().py

--- for i in range().py
+++ for i in range().py
@@ -1,10 +1,23 @@
 # Three positive integers a, b and c where a < b < c satisfy:
 # abc + ab + bc + ca + a + b + c = 2024
 # Find the sum of all the possible different values of c
-import randint
+import math
+import random
 for i in range():
+  a = random.randrange(1,12)
+  b = random.randrange(2,13)
+  c = random.randrange(3,400)
 
-holder = 0 
-for i in range(400):
-  holder += i*1*2
+  x = (a*b*c) + (a*b) + (b*c) + (c*a) + (a) + (b) + (c) 
+
+  if x == 2024:
+    total = total + c
+
+
+#import randint
+#for i in range():
+
+#holder = 0 
+#for i in range(400):
+#  holder += i*1*2
   
